ca79ca3ad77a94d570bef878e96bdce96e170a0c
bernicebetito <bernice_betito@dlsu.edu.ph>
2022-01-24 19:56:10 +0800

    Update tic-tac-toe.py

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -4,36 +4,58 @@ import os
 class TicTacToe:
     def __init__(self):
         self.grid_content = [["     " for _ in range(3)] for _ in range(3)]
+        self.moves = dict()
         self.winner = "Draw"
 
-    def gameContinue(self):
-        if self.grid_content[0][0] != "     ":
-            if self.grid_content[0][0] == self.grid_content[0][1] and self.grid_content[0][0] == self.grid_content[0][2]:
-                self.winner = self.grid_content[0][0]
-                return False
-            elif self.grid_content[0][0] == self.grid_content[1][0] and self.grid_content[0][0] == self.grid_content[2][0]:
-                self.winner = self.grid_content[0][0]
-                return False
-            elif self.grid_content[0][0] == self.grid_content[1][1] and self.grid_content[0][0] == self.grid_content[2][2]:
-                self.winner = self.grid_content[0][0]
-                return False
-            elif self.grid_content[0][0] == self.grid_content[1][2] and self.grid_content[0][0] == self.grid_content[2][2]:
-                self.winner = self.grid_content[0][0]
-                return False
-        elif self.grid_content[1][0] != "     ":
-            if self.grid_content[1][0] == self.grid_content[1][1] and self.grid_content[1][0] == self.grid_content[1][2]:
-                self.winner = self.grid_content[0][0]
-                return False
-        return True
-
-    def getMoves(self):
-        possibleMoves = []
+        count = 1
         for row in range(len(self.grid_content)):
             for col in range(len(self.grid_content[row])):
-                if self.grid_content[row][col] == "     ":
-                    possibleMoves.append((row, col))
+                self.moves[count] = (row, col)
+                count += 1
 
-        return possibleMoves
+    def gameContinue(self):
+        for row in range(3):
+            if list(self.grid_content[row][col] for col in range(3)) == [self.grid_content[row][0] * 3] and self.grid_content[row][0] != "     ":
+                self.winner = self.grid_content[row][0]
+                return False
+
+        for col in range(3):
+            if list(self.grid_content[row][col] for row in range(3)) == [self.grid_content[0][col] * 3] and self.grid_content[0][col] != "     ":
+                self.winner = self.grid_content[0][col]
+                return False
+
+        if self.grid_content[0][0] == self.grid_content[1][1] == self.grid_content[2][2] and self.grid_content[0][0] != "     ":
+            self.winner = self.grid_content[0][0]
+            return False
+
+        if self.grid_content[0][2] == self.grid_content[1][1] == self.grid_content[2][0] and self.grid_content[0][2] != "     ":
+            self.winner = self.grid_content[0][0]
+            return False
+
+        return any("     " in content for content in self.grid_content)
+
+    def getMoves(self):
+        print("      Possible Locations\n")
+
+        loc_count = 1
+        for row in range(len(self.grid_content)):
+            print(end="  ")
+            for col in range(len(self.grid_content[row])):
+                if self.grid_content[row][col] == "     ":
+                    if col == 0:
+                        edge = "   "
+                    else:
+                        edge = "|  "
+                    print(f"{edge}[{loc_count}]", end="  ")
+                else:
+                    if col == 0:
+                        print(end="        ")
+                    else:
+                        print(end="|       ")
+                loc_count += 1
+            print("\n", end="  ")
+            if row < len(self.grid_content) - 1:
+                print("-" * 25)
 
     def playerTurn(self, player, move):
         if player == "X":
@@ -41,62 +63,61 @@ class TicTacToe:
         else:
             piece = "  O  "
 
-        if self.grid_content[move[0]][move[1]] == "     ":
-            self.grid_content[move[0]][move[1]] = piece
-            return True
+        if move in self.moves:
+            if self.grid_content[self.moves[move][0]][self.moves[move][1]] == "     ":
+                self.grid_content[self.moves[move][0]][self.moves[move][1]] = piece
+                return True
         return False
 
     def printBoard(self):
-        print("-" * 40)
-        for i in range(-1, 3):
-            if i >= 0:
-                print("   ", i + 1, " ", end="  |")
-            else:
-                print("         |", end="")
-        print()
-        print("-" * 40)
-        row_ctr = 65
+        print("-" * 30)
         for row in self.grid_content:
-            print("         |" * 4, end="\n")
-            print("   ", chr(row_ctr), "   |", end="")
+            print("         |" * 3, end="\n")
             for column in row:
                 print(" ", column, end="  |")
             print("\n         |", end="")
-            print("         |" * 3, end="\n")
-            print("-" * 40, end="\n")
-            row_ctr += 1
+            print("         |" * 2, end="\n")
+            print("-" * 30, end="\n")
 
 
-tictactoe = TicTacToe()
-turn = ""
+if __name__ == "__main__":
+    try:
+        tictactoe = TicTacToe()
+        turn = ""
 
-while turn.lower() != "x" and turn.lower() != "o":
-    turn = input("Who plays first?\t[X] or [O]:\t")
-    if turn.lower() != "x" and turn.lower() != "o":
-        print("Invalid Key! Please enter [X] or [O]\n")
+        while turn.lower() != "x" and turn.lower() != "o":
+            turn = input("Who plays first?\n[X] or [O]:\t")
+            if turn.lower() != "x" and turn.lower() != "o":
+                print("Invalid Key! Please enter [X] or [O]\n")
 
-while tictactoe.gameContinue():
-    os.system("cls" if os.name == "nt" else "clear")
-    tictactoe.printBoard()
-    print("\n\n")
+        while tictactoe.gameContinue():
+            os.system("cls" if os.name == "nt" else "clear")
+            tictactoe.printBoard()
+            print("\n\n")
 
-    possibleMoves = tictactoe.getMoves()
-    possibleMoves.sort()
-    print("[#]  LOCATION")
-    for curr_piece in range(len(possibleMoves)):
-        piece_formatted = chr(possibleMoves[curr_piece][0] + 65) + str(possibleMoves[curr_piece][1] + 1)
-        print("[{}]    {:<2} ".format(curr_piece + 1, piece_formatted))
-    print()
+            tictactoe.getMoves()
+            playerTurn = False
+            while not playerTurn:
+                player_move = input(f"\nEnter Location of {turn.upper()}:\t")
+                if player_move.isdigit():
+                    if 0 < int(player_move) < 10:
+                        playerTurn = tictactoe.playerTurn(turn.upper(), int(player_move))
+                    else:
+                        print("Invalid location!\n")
+                else:
+                    print("Invalid location!\n")
 
-    playerTurn = False
-    while not playerTurn:
-        player_move = input(f"Enter Location of {turn.upper()}:\t")
-        playerTurn = tictactoe.playerTurn(turn.upper(), possibleMoves[int(player_move) - 1])
+            if turn.upper() == "X":
+                turn = "O"
+            else:
+                turn = "X"
 
-    if turn.upper() == "X":
-        turn = "O"
-    else:
-        turn = "X"
+        os.system("cls" if os.name == "nt" else "clear")
+        tictactoe.printBoard()
 
-tictactoe.printBoard()
-print(f"\nGame Over! {tictactoe.winner} wins!")
+        if tictactoe.winner != "Draw":
+            print(f"\nGame Over! {tictactoe.winner} wins!")
+        else:
+            print(f"\nGame Over! Draw between X and O!")
+    except KeyboardInterrupt:
+        print("\nThank you for playing!")
